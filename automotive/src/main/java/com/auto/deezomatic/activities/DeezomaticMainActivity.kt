419639d5b5c7cacfa7abd6799920cdf2ce008944
Michael Kreich <jaykay2k@hotmail.com>
2023-02-06 15:52:30 +0100

    lots of noobie project restructuring, but slowly getting there

--- automotive/src/main/java/com/auto/deezomatic/activities/DeezomaticMainActivity.kt
+++ automotive/src/main/java/com/auto/deezomatic/activities/DeezomaticMainActivity.kt
@@ -2,9 +2,11 @@ package com.auto.deezomatic.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.auto.deezomatic.R
 
 class DeezomaticMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
